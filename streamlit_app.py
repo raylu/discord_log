2f24dbd58304a8b770001d57538460b58b039c78
raylu <lurayl@gmail.com>
2024-12-31 19:10:11 +0000

    load data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,19 @@
+import json
+
+import pandas
 import streamlit
 
+@streamlit.cache_data
+def load_data():
+	with open('emoji_user_stats.json') as f:
+		user_stats = json.load(f)
+	users = user_stats['users']
+	emojis = user_stats['emojis']
+	return user_stats
+
 streamlit.title('SHR emoji')
+loading_text = streamlit.text('loading data...')
+user_stats = load_data()
+loading_text.empty()
+if streamlit.checkbox('show raw user stats'):
+	streamlit.json(user_stats)
